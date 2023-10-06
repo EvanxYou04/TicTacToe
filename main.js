81479dc3025c9f9b26d62f33363694c5a44f9d70
Evan <evanyou2004@gmail.com>
2023-10-05 22:45:41 -0500

    created 3x3 grid with js.

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log("Hello World!");
+const gridContainer = document.querySelector(".game-container");
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 3; j++) {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid-item");
+    gridItem.id = `${i}-${j}`;
+    gridContainer.appendChild(gridItem);
+  }
+}

27ce71fe19c516b4010638969b70efde4c4014b4
Evan <evanyou2004@gmail.com>
2023-10-09 19:31:51 -0500

    created check winner function.

--- main.js
+++ main.js
@@ -14,6 +14,9 @@ gridItems.forEach((item) => {
   item.addEventListener("click", (e) => {
     if (e.target.textContent === "") {
       e.target.textContent = player;
+      if (checkWinner()) {
+        alert(`${player} wins!`);
+      }
     }
   });
 });
@@ -24,3 +27,42 @@ resetBtn.addEventListener("click", () => {
     item.textContent = " ";
   });
 });
+
+function checkWinner() {
+  //check rows
+  for (let i = 0; i < 3; i++) {
+    if (
+      gridItems[i * 3].textContent === player &&
+      gridItems[i * 3 + 1].textContent === player &&
+      gridItems[i * 3 + 2].textContent === player
+    ) {
+      return true;
+    }
+  }
+  //check columns
+  for (let i = 0; i < 3; i++) {
+    if (
+      gridItems[i].textContent === player &&
+      gridItems[i + 3].textContent === player &&
+      gridItems[i + 6].textContent === player
+    ) {
+      return true;
+    }
+  }
+  //check diagonals
+  if (
+    gridItems[0].textContent === player &&
+    gridItems[4].textContent === player &&
+    gridItems[8].textContent === player
+  ) {
+    return true;
+  }
+  if (
+    gridItems[2].textContent === player &&
+    gridItems[4].textContent === player &&
+    gridItems[6].textContent === player
+  ) {
+    return true;
+  }
+  return false;
+}
